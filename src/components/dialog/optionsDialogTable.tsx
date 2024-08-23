df34953244e6913528f87811cd07bc2254b70c77
Iuri Vilarinho <iuri-vilarinho@hotmail.com>
2024-08-23 10:20:16 -0300

    Ajuste id Pessoa botão

--- src/components/dialog/optionsDialogTable.tsx
+++ src/components/dialog/optionsDialogTable.tsx
@@ -1,26 +1,38 @@
 import { DeleteIcon, EyeIcon, PinIcon } from "lucide-react";
 import { Button } from "../button/button";
 
-const OptionDialogTable = ({ onClose }: { onClose: () => void }) => {
+interface DialogProps {
+  selectId: number;
+  onClose: () => void;
+} 
+
+const OptionDialogTable = ({ selectId, onClose }: DialogProps) => {
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-md shadow-lg">
-
         <div className="mt-2">
-          <Button size="sm" className="w-28">Visualizar <EyeIcon className="ml-3" /></Button>
+          <Button size="sm" className="w-28" onClick={()=>alert(selectId)}>
+            Visualizar <EyeIcon className="ml-3" />
+          </Button>
         </div>
         <div className="mt-2">
-          <Button size="sm" className="w-28">Editar <PinIcon className="ml-8" /></Button>
+          <Button size="sm" className="w-28">
+            Editar <PinIcon className="ml-8" />
+          </Button>
         </div>
         <div className="mt-2">
-          <Button size="sm" className="w-28">Desativar <DeleteIcon className="ml-3" /></Button>
+          <Button size="sm" className="w-28">
+            Desativar <DeleteIcon className="ml-3" />
+          </Button>
         </div>
         <div>
-          <Button size="sm" className="mt-4" onClick={onClose}>Fechar</Button>
+          <Button size="sm" className="mt-4" onClick={onClose}>
+            Fechar
+          </Button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default OptionDialogTable;
--- src/pages/pessoa/components/pessoaLista.tsx
+++ src/pages/pessoa/components/pessoaLista.tsx
@@ -1,106 +1,109 @@
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableRowWihtButton,
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableRowWihtButton,
 } from "@/components/ui/table";
 import { useState } from "react";
 
-import {
-    Pagination
-} from "@/components/ui/pagination";
+import { Pagination } from "@/components/ui/pagination";
 
 import OptionDialogTable from "@/components/dialog/optionsDialogTable";
 import { useGetPessoa } from "@/lib/api/tanstackQuery/pessoa";
 import { Pessoa } from "@/types/pessoa";
 
 export function ListaPessoa() {
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
-    const { data, isLoading, error } = useGetPessoa();
+  const { data, isLoading, error } = useGetPessoa();
 
-    if (isLoading) {
-        return <div>Carregando...</div>; // Exibe uma mensagem de carregamento
-    }
+  if (isLoading) {
+    return <div>Carregando...</div>; // Exibe uma mensagem de carregamento
+  }
 
-    if (error) {
-        return <div>Erro ao carregar os dados.</div>; // Exibe uma mensagem de erro
-    }
+  if (error) {
+    return <div>Erro ao carregar os dados.</div>; // Exibe uma mensagem de erro
+  }
 
-    if (!data || data.length === 0) {
-        return <div>Nenhuma pessoa encontrada.</div>; // Exibe uma mensagem caso não haja dados
-    }
+  if (!data || data.length === 0) {
+    return <div>Nenhuma pessoa encontrada.</div>; // Exibe uma mensagem caso não haja dados
+  }
 
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedPessoa, setSelectedPessoa] = useState<Pessoa | null>(null);
 
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const [selectedPessoa, setSelectedPessoa] = useState<Pessoa | null>(null);
+  const handleButtonClick = (pessoa: Pessoa) => {
+    setSelectedPessoa(pessoa);
+    setIsDialogOpen(true);
+  };
 
-    const handleButtonClick = (pessoa: Pessoa) => {
-        setSelectedPessoa(pessoa);
-        setIsDialogOpen(true);
-    };
+  // Paginação dos dados
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = Math.max(currentPage - 1, 1) * itemsPerPage;
+  const paginatedData = data.slice(startIndex, startIndex + itemsPerPage);
 
-    // Paginação dos dados
-    const totalPages = Math.ceil(data.length / itemsPerPage);
-    const startIndex = Math.max((currentPage - 1), 1) * itemsPerPage;
-    const paginatedData = data.slice(startIndex, startIndex + itemsPerPage);
-
-    console.log(currentPage)
-    return (
-        <div className="flex h-full justify-center w-full p-10">
-            <div className="w-4/5">
-                <Table className="bg-slate-50">
-                    <TableCaption>A list of your recent invoices.</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="">Nome</TableHead>
-                            <TableHead>Email</TableHead>
-                            <TableHead>Telefone</TableHead>
-                            <TableHead className="">Data Nascimento</TableHead>
-                            <TableHead>Placa Veiculo</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {paginatedData.map((pessoa: Pessoa) => (
-                            <TableRowWihtButton
-                                key={pessoa.id}
-                                onButtonClick={() => handleButtonClick(pessoa)}
-                            >
-                                <TableCell className="font-medium">{pessoa.nomeCompleto}</TableCell>
-                                <TableCell>{pessoa.email}</TableCell>
-                                <TableCell>{pessoa.telefoneCelular}</TableCell>
-                                <TableCell className="">{pessoa.dataNascimento}</TableCell>
-                                <TableCell>{pessoa.informacaoSeguranca?.placaVeiculo || 'N/A'}</TableCell>
-
-                            </TableRowWihtButton>
-                        ))}
-                        {isDialogOpen && (
-                            <OptionDialogTable
-                                onClose={() => setIsDialogOpen(false)} />
-                        )}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow>
-                            <TableCell colSpan={5}>
-                                <div className="flex justify-center">
-                                    <Pagination
-                                        currentPage={currentPage}
-                                        totalPages={totalPages}
-                                        onPageChange={setCurrentPage}
-                                    >
-                                    </Pagination>
-                                </div>
-                            </TableCell>
-                        </TableRow>
-                    </TableFooter>
-                </Table>
-            </div>
-        </div>
-    );
+  console.log(currentPage);
+  return (
+    <div className="flex h-full justify-center w-full p-10">
+      <div className="w-4/5">
+        <Table className="bg-slate-50">
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="">Nome</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Telefone</TableHead>
+              <TableHead className="">Data Nascimento</TableHead>
+              <TableHead>Placa Veiculo</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {paginatedData.map((pessoa: Pessoa) => (
+              <TableRowWihtButton
+                key={pessoa.id}
+                onButtonClick={() => handleButtonClick(pessoa)}
+              >
+                <TableCell className="font-medium">
+                  {pessoa.nomeCompleto}
+                </TableCell>
+                <TableCell>{pessoa.email}</TableCell>
+                <TableCell>{pessoa.telefoneCelular}</TableCell>
+                <TableCell className="">{pessoa.dataNascimento}</TableCell>
+                <TableCell>
+                  {pessoa.informacaoSeguranca?.placaVeiculo || "N/A"}
+                </TableCell>
+              </TableRowWihtButton>
+            ))}
+            {isDialogOpen && (
+              <OptionDialogTable
+                selectId={selectedPessoa?.id ?? 0}
+                onClose={() => {
+                  setIsDialogOpen(false);
+                }}
+              />
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={5}>
+                <div className="flex justify-center">
+                  <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={setCurrentPage}
+                  ></Pagination>
+                </div>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </div>
+  );
 }
